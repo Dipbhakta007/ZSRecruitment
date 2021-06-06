50aee9e0ad15a0e6877865090b35d30479e8e6dc
Dip <bhaktadip@gmail.com>
2021-06-07 00:21:55 +0600

    Add API schema

--- address_book_api/urls.py
+++ address_book_api/urls.py
@@ -7,12 +7,14 @@
 
 from django.urls import path
 from .views import AddressDetail, CountryList, StateList, AddressList
+from rest_framework.schemas import get_schema_view
 
 app_name='address_book_api'
 
 urlpatterns = [
-    path('countries/', CountryList.as_view(), name='countrylistcreate'),                # Url to get list of all countries
-    path('states/<str:country_code>/', StateList.as_view(), name='statelistcreate'),         # Url to get list of all states of the country specified in the url by country ISO code (https://countrycode.org/)
-    path('addresses/<str:state_name>/', AddressList.as_view(), name='addresslistcreate'),     # Url to get list of addresses of the state specified in the url by the state name
+    path('countries/', CountryList.as_view(), name='countrylistcreate'),                 # Url to get list of all countries
+    path('states/<str:country_code>/', StateList.as_view(), name='statelistcreate'),     # Url to get list of all states of the country specified in the url by country ISO code (https://countrycode.org/)
+    path('addresses/<str:state_name>/', AddressList.as_view(), name='addresslistcreate'),# Url to get list of addresses of the state specified in the url by the state name
     path('address-detail/<int:pk>/', AddressDetail.as_view(), name='addressdetailview'), # Url to get a detailed view of a specific address specified in the url by its id
+    path('schema/', get_schema_view(title='ZSRecruitment', description='Project for ZS Solutions Recruitment Process', version='1.0.0'), name='openapi-schema')
 ]
